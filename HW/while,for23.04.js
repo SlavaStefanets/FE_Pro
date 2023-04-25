1e3b69dd8de85e5527e40f760575c4643550e300
slavastefanets <jesusbigboss12@gmail.com>
2023-04-25 17:57:01 +0200

    25.04

---
+++ HW/while,for23.04.js
@@ -0,0 +1,36 @@
+// let a = 0;
+// while(a <= 100){
+//     console.log(a);
+//     a += 5
+// }
+
+// 2 ----------
+
+// for(let a = 1900; a <= 2023; a += 4){
+//     console.log(a);
+// }
+
+// 3 ------------
+
+// for(let a = 0; a <= 100; a += 1){
+//     if(a % 2=0)
+//     console.log(a);
+// }
+
+// 4 ------------
+
+// let word = 'hello'
+// for(let i = 5; i >= 0; i --){
+//     console.log(word[i]);
+// }
+
+// 5 ---------
+
+let word = 'hello'
+let reversedWord = ''
+
+    for(let i = word.length ; i >= 0; i --){
+        reversedWord = word[i]
+        console.log(reversedWord);
+        
+};
---
+++ LS/Lessons5/for/while/doWhile.js
@@ -0,0 +1,68 @@
+//While
+
+// 0 to 49
+
+// let i = 0;
+// while(i < 50){
+//     console.log(i)
+//     i = i + 1
+//     //ili i += 1
+// }
+
+// ot 10 do 0
+
+// let i = 10;
+// while(i >= 0){
+//     console.log(i)
+//     i = i - 1
+// }
+
+// let i = 2;
+// while(i <= 10){
+//     console.log(i)
+//     i += 2
+// }
+
+// let i = -10;
+// while(i <= 10){
+//     console.log(i)
+//     i += 1
+// }
+
+//for
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// --------------
+// if inside for
+
+// for (let i = 0; i <= 10; i++){
+//     if(i % 2 == 0)
+//     console.log(i)  //4etn6e 2 4 6
+// }
+
+// for (let i = 0; i <= 10; i++){
+//     if(i % 2 != 0)
+//     console.log(i)  //ne 4etn6e 1 3 5
+// }
+
+// --------------
+//ot 0 do 10 bez 5,8
+
+// for ( let i = 0; i <= 10; i++){
+//     if (i != 5 && i != 8)
+//     console.log(i);
+// }
+// -------------
+
+// for ( let i = 0; i <= 100; i++){
+//         if (Math.sqrt(i) % 1 == 0)
+//         console.log(i);
+//     } // esli v64isljaetsja kvadratn6j korenj
+
+// let string = 'world'
+// for (let i = 0; i <= 5; i++){
+// console.log(string[i])
+// }
