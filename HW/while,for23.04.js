7e0a96e16f315dbdb5b5cd5d1cca273cf8bccc9f
slavastefanets <jesusbigboss12@gmail.com>
2023-04-26 20:24:23 +0200

    26.04

--- HW/while,for23.04.js
+++ HW/while,for23.04.js
@@ -12,10 +12,24 @@
 
 // 3 ------------
 
-// for(let a = 0; a <= 100; a += 1){
-//     if(a % 2=0)
-//     console.log(a);
-// }
+
+for(let a = 2; a <= 100; a ++) {
+    let isPrime = true;
+
+
+for(let b =2; b < a; b ++) {
+     if (a % b == 0){
+     isPrime = false;
+     break; } }
+
+    if (isPrime) {
+
+     console.log(a)
+}}
+
+
+
+
 
 // 4 ------------
 
@@ -26,11 +40,15 @@
 
 // 5 ---------
 
-let word = 'hello'
-let reversedWord = ''
+// let word = 'hello';
+// let reversed = '';
 
-    for(let i = word.length ; i >= 0; i --){
-        reversedWord = word[i]
-        console.log(reversedWord);
+//     for(let i = word.length -1 ; i >= 0; i --){
+//         reversed += word[i]
+//     };
+//         console.log(reversed)
+
         
-};
+
+ 
+
---
+++ LS/Lessons6/funcion.js
@@ -0,0 +1,85 @@
+// console.log('-_'.repeat(10));
+// console.log('Hallo user');
+// console.log('-_'.repeat(10));
+
+// function sayHallo(){    //'function sozdanie' 
+//      console.log('-_'.repeat(10));
+//      console.log('Hallo user');
+//      console.log('-_'.repeat(10));
+// }
+
+// sayHallo()   //open function
+
+// sayHallo()
+
+// sayHallo()
+
+// ---------------
+
+// аргументы function
+//bez argumentov !!!!
+
+
+//  function sayHalloTigran(){    
+//          console.log('-_'.repeat(10));
+//          console.log('Hallo user Tigran');
+//          console.log('-_'.repeat(10));
+//  }
+
+//  function sayHalloAlex(){    
+//     console.log('-_'.repeat(10));
+//     console.log('Hallo user Alex');
+//     console.log('-_'.repeat(10));
+// }
+
+//function s argumentami !!!!
+
+// function sayHallo(name){
+//      console.log('-_'.repeat(10));
+//      console.log('Hallo user ' + name);  //ili ${name}'
+//      console.log('-_'.repeat(10));
+// }
+// sayHallo('slava')
+// sayHallo('Alex')
+
+// ---------------
+
+//drugoj primer
+
+// function sum (a, b){
+//     console.log(a + b);
+// }
+
+// sum(5, 10)
+// sum(true, 10)
+// sum(false, 10)
+
+// function double (num){     //praktika
+//     console.log(num ** 2);
+// }
+// double(3)
+
+
+function checkString(str){
+    if (typeof(str) === 'string'){
+    console.log('это строка')
+}  else {
+    console.log('не строка')
+}}
+
+checkString('34')
+
+// function checkMath(a, b) {
+//     if (a % b == 0){
+//     console.log('delitsja');
+// } else {
+//     console.log('Ne delitsja');
+// }}
+
+// checkMath(4, 3)
+
+
+// function checkMath (a, b){
+//    console.log((a % b == 0) ? 'delitsja' : 'Ne delitsja')
+// }
+// checkMath(3, 3)
