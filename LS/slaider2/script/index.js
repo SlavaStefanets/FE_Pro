3f9e292b27d1c9b3dd5164a00ae36a17073d5990
slavastefanets <jesusbigboss12@gmail.com>
2023-10-12 19:20:59 +0200

    new

---
+++ LS/slaider2/script/index.js
@@ -0,0 +1,11 @@
+const modal_container = document.querySelector(".modal_container");
+const openBtn = document.querySelector(".open");
+const closeBtn = document.querySelector(".close");
+
+openBtn.addEventListener("click", () => {
+  modal_container.classList.remove("close_status");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal_container.classList.add("close_status")
+});
